024512ecb5775fac1e2aaf5d68de468afe8cc5a3
Biancalaureano <biancaalmeidabi23@hotmail.com>
2024-11-28 15:31:51 -0300

    adicionada função de troca de tema e salvando preferencia em Cookie

---
+++ src/main/java/com/PI3/SitePousada/model/Preferencia.java
@@ -0,0 +1,20 @@
+
+package com.PI3.SitePousada.model;
+
+
+
+public class Preferencia {
+    
+    private String tema; 
+
+    public String getTema() {
+        return tema;
+    }
+
+    public void setTema(String tema) {
+        this.tema = tema;
+    }
+    
+    
+    
+}
